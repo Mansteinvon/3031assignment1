5b7e28d6379f38e15996c9aa00163123825a7dce
Mansteinvon <36380930+Mansteinvon@users.noreply.github.com>
2023-07-29 08:38:38 -0700

    "adding handler class

---
+++ project/src/main/java/ca/yorku/eecs/Handler.java
@@ -0,0 +1,26 @@
+package ca.yorku.eecs;
+
+import java.io.*;
+import java.net.*;
+
+import java.util.*;
+
+import com.sun.net.httpserver.*;
+
+
+public class Handler implements HttpHandler{
+
+	
+	
+	
+	
+	
+	
+	
+	@Override
+	public void handle(HttpExchange exchange) throws IOException {
+		
+		
+	}
+
+}
